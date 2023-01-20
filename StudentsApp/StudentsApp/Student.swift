86ba85d2ad8e8a3d77f9f6c16a34303ef03d81d1
RaniaArbash <41278913+RaniaArbash@users.noreply.github.com>
2023-01-20 15:28:57 -0500

    Add files via upload

---
+++ StudentsApp/StudentsApp/Student.swift
@@ -0,0 +1,23 @@
+//
+//  Student.swift
+//  StudentsApp
+//
+//  Created by Rania Arbash on 2023-01-20.
+//
+
+import Foundation
+
+
+struct Student {
+    var name : String
+    var number : String
+    var age : Int
+}
+
+
+var studentList = [
+    Student(name: "Mary", number: "1234333", age: 23),
+    Student(name: "John", number: "2019201", age: 22),
+    Student(name: "Lee", number: "3828193", age: 30)
+
+]
---
+++ StudentsApp/StudentsApp/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  StudentsApp
+//
+//  Created by Rania Arbash on 2023-01-20.
+//
+
+import UIKit
+
+// MVC
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var numberText: UILabel!
+    @IBOutlet weak var nameText: UILabel!
+    
+    @IBOutlet weak var ageText: UILabel!
+
+    var allStudentsArray = studentList
+    var index = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI(currentIndex: index)
+    }
+
+    @IBAction func nextClicked(_ sender: Any) {
+        index += 1
+        if index < allStudentsArray.count{
+            updateUI(currentIndex: index)
+        }else {
+            index = 0
+            updateUI(currentIndex: index)
+        }
+            
+    }
+    
+    @IBAction func prevClicked(_ sender: Any) {
+        index -= 1
+        if (index < allStudentsArray.count) && (index > 0) {
+            updateUI(currentIndex: index)
+        }else {
+            index = 0
+            updateUI(currentIndex: index)
+        }
+            
+    }
+    
+    func updateUI(currentIndex : Int) {
+        nameText.text = "Name :" + allStudentsArray[currentIndex].name
+        numberText.text = "Numbe :" + allStudentsArray[currentIndex].number
+        ageText.text =  " Age : \(allStudentsArray[currentIndex].age)"
+    }
+    
+}
+
